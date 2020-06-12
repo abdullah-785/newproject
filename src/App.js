0237f2b96bed1e91de261ed041c78535895f59c0
Abdullah <60250106+abdullah-785@users.noreply.github.com>
2020-06-12 21:07:54 +0500

    Updated to create some button
    To create the increase count, decrease count, reset count and also create mornig and night function buttons.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Message from './message.js';
+//import Sweet from './sweet.js';
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
-      Hii Abdullah Butt. Finally you are successfull to create the reate app
-    </div>
+  var a=1;
+  let [count, setCount] = useState(a);
+  let [isMorning, setMorning] = useState(true);
+  return(
+          <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+            <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+            <button onClick={()=> setMorning(!isMorning)}>
+              Update Day
+            </button>
+
+            <Message counter={count}/>
+            <button onClick={()=> setCount(++count)}>
+              Update count
+            </button>
+            <br></br>
+            <br></br>
+            <button onClick={()=> setCount(--count) }>
+              Decrease count
+            </button>
+            <br></br>
+            <br></br>
+            <button onClick={()=> setCount(a)}>
+              Reset count
+            </button>
+          </div>
+
   );
 }
+
 
 export default App;
